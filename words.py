4c64aa6ed41beb99579faf1b6f5f2c6f99caf7e2
Nicholas Jakubowski <nicholas.jakubowski@accenture.com>
2018-05-07 14:14:42 -0700

    Pass list to print function

--- words.py
+++ words.py
@@ -7,8 +7,12 @@ def fetch_words():
             line_words = line.decode('utf-8').split()
             for word in line_words:
                 story_words.append(word)
+    return story_words
 
+def print_words(story_words)
     for word in story_words:
         print(word)
 
-print(__name__)
+if __name__ == '__main__':
+    words = fetch word()
+    print_words(words)
