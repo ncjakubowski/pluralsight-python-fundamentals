ef1106ce40ca73f36068778617ba961d6ebdd479
Nicholas Jakubowski <nicholas.jakubowski@accenture.com>
2018-05-07 14:35:09 -0700

    Pass arg to file when run via cmd line

--- words.py
+++ words.py
@@ -1,7 +1,9 @@
+import sys
 from urllib.request import urlopen
 
-def fetch_words():
-    with urlopen('http://sixty-north.com/c/t.txt') as story:
+# http://sixty-north.com/c/t.txt
+def fetch_words(url):
+    with urlopen(url) as story:
         story_words = []
         for line in story:
             line_words = line.decode('utf-8').split()
@@ -13,9 +15,11 @@ def print_items(story_words):
     for word in story_words:
         print(word)
 
-def main():
-    words = fetch_words()
+def main(url):
+    # pass arguments from cmd line
+    # url = sys.argv[1]
+    words = fetch_words(url)
     print_items(words)
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1])
