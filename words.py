5af6b9cc345b07b182309328ce1918cd5cf55e82
Nicholas Jakubowski <nicholas.jakubowski@accenture.com>
2018-05-07 14:40:46 -0700

    Add extra lines to conform to PEP eight recommendation

--- words.py
+++ words.py
@@ -11,15 +11,18 @@ def fetch_words(url):
                 story_words.append(word)
     return story_words
 
+
 def print_items(story_words):
     for word in story_words:
         print(word)
+
 
 def main(url):
     # pass arguments from cmd line
     # url = sys.argv[1]
     words = fetch_words(url)
     print_items(words)
+    
 
 if __name__ == '__main__':
     main(sys.argv[1])
